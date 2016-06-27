ecc9bdab2eca8c05ee980086faa6d094a31bf967
Justin Chan <justinchanuk@gmail.com>
2016-06-27 12:31:39 +0000

    Pass Test: 4 Remaining

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user, only: [:create, :edit]
 
   # GET /posts
   # GET /posts.json
